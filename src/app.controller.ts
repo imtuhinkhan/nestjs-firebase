927488662fd6c69f93b956db07994117a9c96f23
Tuhin <uiu.tuhin@gmail.com>
2023-01-04 16:02:17 +0600

    Connect Firebase

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, Body, Delete, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,25 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): {} {
     return this.appService.getHello();
+  }
+
+  @Get('/user')
+  getUser(): {} {
+    return this.appService.getUser();
+  }
+
+  @Post('/user')
+    async addUser(
+        @Body("name") name: string,
+        @Body("email") email: string
+    ) {
+    try {
+      const res = await this.appService.addUser(name, email)
+      return res
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
-
+import * as admin from 'firebase-admin';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): {} {
+    return {'product':'Teton Bath Scale'};
+  }
+
+  async getUser(): Promise<any> {
+    try {
+      let data = [];
+      let userList = await  admin.firestore().collection('Users').get();
+      userList.forEach((doc) => {
+        data.push(doc.data());
+      })
+      return data;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async addUser(name: string, email: string): Promise<any> {
+    try {
+      const data = {
+        email, name
+      }
+      const res = await admin.firestore().collection("Users").add(data)
+      return res;
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
